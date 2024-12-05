5f51252b2036ffb72ba7ab2cd232bd2226e1acb6
TreanT <hntreant23@gmail.com>
2024-12-05 14:11:07 +0700

    base online services

--- online-services/src/app.module.ts
+++ online-services/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { LoggerModule } from './logger/logger.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionFilter } from './filter/exception.filter';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, LoggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService, 
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter
+    }
+  ],
 })
 export class AppModule {}
---
+++ online-services/src/core/database/database.config.ts
@@ -0,0 +1,66 @@
+import * as dotenv from "dotenv";
+
+import { IDatabaseConfig } from "./interfaces/dbConfig.interface";
+
+dotenv.config();
+
+export const databaseConfig: IDatabaseConfig = {
+   development: {
+    username: "root",
+    password: "hnam23012002",
+    database: "auth_service",
+    host: "127.0.0.1",
+    port: 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      // useUTC: false, // for reading from database
+      dateStrings: true,
+      typeCast(field: any, next: any) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+
+    dialectOptions: {
+      // useUTC: false, // for reading from database
+      dateStrings: true,
+      typeCast(field: any, next: any) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+
+    dialectOptions: {
+      // useUTC: false, // for reading from database
+      dateStrings: true,
+      typeCast(field: any, next: any) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
+  },
+};
